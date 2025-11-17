c887bf736ecd4ef392db97fa748d1e3c63ebb1ef
Alwande Ngcobo ST10050768 <130943658+ST10050768@users.noreply.github.com>
2025-11-17 18:02:30 +0200

    Update Firebase service account JSON file path

--- client/firebase-admin.js
+++ client/firebase-admin.js
@@ -1,5 +1,5 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('../prog7314poe-firebase-adminsdk-fbsvc-f157dfd39d.json');
+const serviceAccount = require('../prog7314poe-firebase-adminsdk-fbsvc-22ecf7e4af.json');
 
 // Prevent crash when admin.apps is undefined during mocking
 if (!admin.apps || admin.apps.length === 0) {
