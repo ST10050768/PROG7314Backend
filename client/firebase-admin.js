b1fa3dd5c091a96405a35bc593da74183e08df2a
Alwande Ngcobo ST10050768 <130943658+ST10050768@users.noreply.github.com>
2025-11-17 18:32:10 +0200

    Add console log for Firebase private key ID
    Log the private key ID of the Firebase service account.

--- client/firebase-admin.js
+++ client/firebase-admin.js
@@ -1,6 +1,8 @@
 const admin = require('firebase-admin');
 const serviceAccount = require('../prog7314poe-firebase-adminsdk-fbsvc-22ecf7e4af.json');
 
+console.log("Using Firebase key:", serviceAccount.private_key_id);
+
 // Prevent crash when admin.apps is undefined during mocking
 if (!admin.apps || admin.apps.length === 0) {
   admin.initializeApp({
